75afc397a8a1ea3e92da089f03dc05e4a6383a93
Afaan Naqvi <Afaan.Naqvi@katerra.com>
2018-02-20 11:14:28 -0800

    Update compute.py to write df to csv. Add plot.py for plotting

--- hdd_cdd_compute.py
+++ hdd_cdd_compute.py
@@ -53,29 +53,11 @@ def main_func():
 		extract_data[i] = pd.Series(series_lists[index])
 	# Add a total HDD, CDD column to the df
 	extract_data['TDD'] = extract_data['HDD'] + extract_data['CDD']
+	extract_data.sort_values(['TDD'], inplace=True)
+	# Write resulting dataframe to file
+	extract_data.to_csv('hdd_cdd.csv')
 
 main_func()
 
 
 print(extract_data)
-
-# Set plot tools
-tool_bar = ['box_zoom','pan','save', 'reset', 'wheel_zoom', 'hover', 'crosshair']
-
-# Loop to create 3 bokeh plots (TDD ascending, HDD ascending, and CDD ascending)
-for param, html_file in zip(['TDD', 'HDD', 'CDD'], ['TDD_Ascending.html', 'HDD_Ascending.html', 'CDD_Ascending.html']):
-	# Sort extract_data df in place by ascending HDD, TDD, and CDD
-	extract_data.sort_values([param], inplace=True)
-	# Set ouput file
-	output_file(html_file)
-	# Create chart
-	line_chart = figure(x_range=list(extract_data['File']), title='TMY Station HDD CDD Spread', x_axis_label='Station', tools=tool_bar)
-	# Create data lines for chart
-	for i, j in zip(['HDD', 'CDD', 'TDD'], ['#ff0000', '#0000ff', 'Black']):
-		line_chart.line(list(range(0,len(extract_data[i]))), list(extract_data[i]), legend=i, color=j)
-	# Set chart scaling
-	line_chart.sizing_mode='scale_both'
-	# Remove vertical grid lines
-	line_chart.xgrid.grid_line_color = None
-	# Show chart
-	show(line_chart)
---
+++ hdd_cdd_plot.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from bokeh.io import show, output_file
+from bokeh.plotting import figure, show, output_file
+from tmy_header_city_state_extractor import get_head
+
+extract_data = pd.read_csv('hdd_cdd.csv')
+
+# Set plot tools
+tool_bar = ['box_zoom','pan','save', 'reset', 'wheel_zoom', 'hover', 'crosshair']
+
+# Loop to create 3 bokeh plots (TDD ascending, HDD ascending, and CDD ascending)
+for param, html_file in zip(['TDD', 'HDD', 'CDD'], ['TDD_Ascending.html', 'HDD_Ascending.html', 'CDD_Ascending.html']):
+	# Sort extract_data df in place by ascending HDD, TDD, and CDD
+	extract_data.sort_values([param], inplace=True)
+	# Set ouput file
+	output_file(html_file)
+	# Create chart
+	line_chart = figure(x_range=list(extract_data['File']), title='TMY Station HDD CDD Spread', x_axis_label='Station', tools=tool_bar)
+	# Create data lines for chart
+	for i, j in zip(['HDD', 'CDD', 'TDD'], ['#ff0000', '#0000ff', 'Black']):
+		line_chart.line(list(range(0,len(extract_data[i]))), list(extract_data[i]), legend=i, color=j)
+	# Set chart scaling
+	line_chart.sizing_mode='scale_both'
+	# Remove vertical grid lines
+	line_chart.xgrid.grid_line_color = None
+	# Show chart
+	show(line_chart)
